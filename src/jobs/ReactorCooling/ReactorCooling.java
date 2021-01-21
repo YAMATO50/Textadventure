3b9be54ebe672d853c40b5f97b75668a767334a7
YAMATO50 <noah.kuemmerle@gmail.com>
2021-01-21 12:28:41 +0100

    Adjusted Timings

--- src/jobs/ReactorCooling/ReactorCooling.java
+++ src/jobs/ReactorCooling/ReactorCooling.java
@@ -23,9 +23,9 @@ public class ReactorCooling {
 		TextAdventure.wait(1000);
 		System.out.println("Auf seinem Schildchen an seinem anzug steht \"Tobias Ingram, Reaktor Leiter\"");
 		TextAdventure.wait(1000);
-		TextTools.slowTextln("Hi, " + Config.save.playerName + " Herzlich Willkommen in der Reaktorkuppel!", 75);
-		TextTools.slowTextln("Ich bin Tobias Ingram, der Leiter der Reaktorkuppel.", 75);
-		TextTools.slowTextln("Du bist also hier um die Reaktorkühlung zu prüfen. Hast du das schonmal gemacht?", 75);
+		TextTools.slowTextln("Hi, " + Config.save.playerName + " Herzlich Willkommen in der Reaktorkuppel!", 30);
+		TextTools.slowTextln("Ich bin Tobias Ingram, der Leiter der Reaktorkuppel.", 30);
+		TextTools.slowTextln("Du bist also hier um die Reaktorkühlung zu prüfen. Hast du das schonmal gemacht?", 30);
 		System.out.println();
 		
 		System.out.println("[1] Ja");
@@ -43,8 +43,8 @@ public class ReactorCooling {
 		System.out.println();
 		
 		if (answer == 1) {
-			TextTools.slowTextln("Ich kann mir einfach nie merken wer schonmal hier war und wer nicht!", 75);
-			TextTools.slowTextln("Na dann leg los! Du weißt ja wie es geht.", 75);
+			TextTools.slowTextln("Ich kann mir einfach nie merken wer schonmal hier war und wer nicht!", 30);
+			TextTools.slowTextln("Na dann leg los! Du weißt ja wie es geht.", 30);
 		} else {
 			TextTools.slowTextln("Okay, dann erkläre ich dir mal alles!"
 					+ "\n" + "Dort drüben an dem Rechner kommen alle Informationen der Kühlanlage an"
@@ -57,7 +57,7 @@ public class ReactorCooling {
 					+ "\n" + "Wenn du fertig bist, und alle werte in ordnung sind, rufe die \"update.exe\" auf. Du kannst dann wieder zu mir kommen und dein Geld"
 					+ "\n" + "abholen. Aber denk drann! Wenn einer oder mehrere der Werte nicht passen, bekommst du kein Geld!"
 					+ "\n" + "Falls dir etwas zu schnell war, schau einfach in die \"help.txt\"."
-					+ "\n" + "Also dann! Viel erfolg!", 75);
+					+ "\n" + "Also dann! Viel erfolg!", 30);
 		}
 		System.out.println();
 		System.out.println("Du gehst zum Computer, setzt dich hin und blickst auf den Bildschirm");
@@ -73,10 +73,10 @@ public class ReactorCooling {
 		
 		if (Updater.succes) {
 			System.out.println("Du stehst auf und gehst zu Tobias Ingram.");
-			TextAdventure.wait(1500);
-			TextTools.slowTextln("Oh du bist fertig! Sehr schön. Dann lass mich mal sehen ob du alles richtig gemacht hast.",75);
+			TextAdventure.wait(1000);
+			TextTools.slowTextln("Oh du bist fertig! Sehr schön. Dann lass mich mal sehen ob du alles richtig gemacht hast.",30);
 			System.out.println("Du folgst ihm zu einem Terminal. Er tippt irgendwass ein und ruft eine datei auf.");
-			TextAdventure.wait(1500);
+			TextAdventure.wait(1000);
 			System.out.println("Nach dem er sich die datei kurz angesehen hat dreht er sich um zu dir.");
 			TextTools.slowTextln("Sehr gut! Alles ist richtig eingestellt. Komm gerne wieder. Dein Gehalt überweise ich dir!"
 					+ "\n" + "Hab noch einen schönen tag!");
@@ -106,10 +106,10 @@ public class ReactorCooling {
 			
 		} else {
 			System.out.println("Du stehst auf und gehst zu Tobias Ingram.");
-			TextAdventure.wait(1500);
-			TextTools.slowTextln("Oh du bist fertig! Sehr schön. Dann lass mich mal sehen ob du alles richtig gemacht hast.",75);
+			TextAdventure.wait(1000);
+			TextTools.slowTextln("Oh du bist fertig! Sehr schön. Dann lass mich mal sehen ob du alles richtig gemacht hast.",30);
 			System.out.println("Du folgst ihm zu einem Terminal. Er tippt irgendwass ein und ruft eine datei auf.");
-			TextAdventure.wait(1500);
+			TextAdventure.wait(1000);
 			System.out.println("Nach dem er sich die datei kurz angesehen hat dreht er sich um zu dir.");
 			TextTools.slowTextln("Du willst fertig sein?! Definitiv nicht! Hier ist mindesten ein wert nicht korrekt eingestellt!"
 					+ "\n" + "Dafür kann ich dich nicht bezahlen! Ich muss jetzt noch jemanden suchen der das hier in Ordnung bringt..."
@@ -133,12 +133,12 @@ public class ReactorCooling {
 						break;
 					case 2:
 						System.out.println("Du gehst dem Leiter hinterher und sagts dass es einen Fehler gab");
-						TextTools.slowTextln("hmmm... Ich werde das prüfen.", 75);
+						TextTools.slowTextln("hmmm... Ich werde das prüfen.", 30);
 						System.out.println("Ihr geht zu einem anderen Terminal. Der Leiter ruft wieder eine datei auf. Es sieht aus wie eine Logdatei");
-						TextAdventure.wait(1500);
+						TextAdventure.wait(1000);
 						System.out.println("Er tippt etwas ein, und dreht sich wieder zu dir");
 						TextTools.slowTextln("Tatsächlich, du hast recht. Bitte entschuldige meine Reaktion. Ich werde dir dein Gehalt überweisen."
-								+ "\n" + "Einen schönen tag dir noch!", 75);
+								+ "\n" + "Einen schönen tag dir noch!", 30);
 						break;
 					default:
 						System.out.println("Bitte gib eine gültige zahl ein!");
@@ -187,7 +187,7 @@ public class ReactorCooling {
 				TextTools.slowTextln("Wir Teilen uns das Reservoir auch mit anderen bereichen die kühlwasser brauchen. Wenn unser teil, also"
 						+ "\n" + "die 100%, voll ist, dann geben wir das an die anderen bereiche Kühlwasser ab. Bei 120% würden wir also die 20%"
 						+ "\n" + "an die anderen bereiche abgeben."
-						,75);
+						,30);
 				break;
 			case 2:
 				TextTools.slowTextln("Den Reaktor muss man sehr vorsichtig behandeln. Wenn man zu stark kühlt, und die Plasmatemperatur"
@@ -195,10 +195,10 @@ public class ReactorCooling {
 						+ "\n" + "Ausserdem wäre das neu starten des Reaktors sehr Energieaufwändig. Es würde Monate dauern, bis die Basis"
 						+ "\n" + "wieder normal funkionieren könnte. Zum glück sind die wichtigsten lebenserhaltenden Systeme Auch Solar,"
 						+ "\n" + "Batterie und Dieselbetrieben."
-						, 75);
+						, 30);
 				break;
 			case 3:
-				TextTools.slowTextln("Alles klar. Einen schönen tag noch!", 75);
+				TextTools.slowTextln("Alles klar. Einen schönen tag noch!", 30);
 				stay = false;
 			}
 			
