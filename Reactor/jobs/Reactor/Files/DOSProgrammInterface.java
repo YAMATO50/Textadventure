720b52f827a148cf1d32f925f6809f7f2a78844f
YAMATO50 <noah.kuemmerle@gmail.com>
2021-07-19 22:56:29 +0200

    new Interface for Reactor Job

---
+++ Reactor/jobs/Reactor/Files/DOSProgrammInterface.java
@@ -0,0 +1,11 @@
+package jobs.Reactor.Files;
+
+public interface DOSProgrammInterface {
+
+	public void execute();
+	
+	public String[] getContents();
+	
+	public void setContents(String[] content);
+	
+}
