75892c20586ae3d11ec79416beaea1c2e8ec4bd2
YAMATO50 <noah.kuemmerle@gmail.com>
2021-07-19 22:13:04 +0200

    codestyle

--- src/jobs/ReactorCooling/Files/DOSProgrammInterface.java
+++ src/jobs/ReactorCooling/Files/DOSProgrammInterface.java
@@ -3,7 +3,9 @@ package jobs.ReactorCooling.Files;
 public interface DOSProgrammInterface {
 
 	public void execute();
+	
 	public String[] getContents();
+	
 	public void setContents(String[] content);
 	
 }
